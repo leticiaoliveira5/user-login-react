f610ff759a5a3bddffa8206c66dd771cff3ee517
Leticia Oliveira <olivaleticia@hotmail.com>
2025-03-12 09:20:02 -0300

    fix eslint complaints

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Main from '../template/Main'
 
-export default props =>
+const Home = props =>
   <Main icon="home" title="Home Page" subtitle="React">
     <div className='display-4'>Bem-vindo</div>
     <br />
     <p className='mb-0'>Sistema teste para exemplificar</p>
   </Main>
+
+export default Home
--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import UserCrud from "../components/user/UserCrud";
 import Home from "../components/home/Home";
 
-export default props => (
+const RoutesComponent = props => (
   <Routes>
     <Route exact path='/' element={<Home/>} />
     <Route path='/users' element={<UserCrud/>} />
     <Route path='*' element={<Home/>} />
   </Routes>
 );
+
+export default RoutesComponent
